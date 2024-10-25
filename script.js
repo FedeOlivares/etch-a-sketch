a66af3094395fd14c680ff55d2cb90b11e91aef9
Federico Olivares <119693761+FedeOlivares@users.noreply.github.com>
2024-10-25 20:44:30 +0000

    Added div creating logic. Fix view sizing

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
 const view = document.querySelector(".view"); 
 const inp = document.getElementById("myRange");
-const side = inp.value; 
+const side = inp.value;
 
 const totalDivs = side * side; 
 
 
 const slider = document.querySelector(".slider");
 
+function createDivs(totalDivs) {
+    view.innerHTML = "";
+    for (let i = 1; i<=totalDivs; i++) {
+        const div = document.createElement("div");
+        div.classList.add("functionDiv");
+        div.innerText = "";
+        view.appendChild(div);
+    }
+}
 
 function updateSliderValue() {
-    document.documentElement.style.setProperty('--slider-value', `${slider.value}px`);
+    view.style.setProperty('--slider-value', slider.value);
+    createDivs(totalDivs);
 }
 
 slider.addEventListener("input", updateSliderValue);
 
 
- window.onload = () => {
-    for (let i = 1; i<=totalDivs; i++) {
-        const div = document.createElement("div");
-        div.classList.add("functionDiv");
-        view.appendChild(div);
-    }
-}
+window.onload = () => { createDivs }
+
+
+
+
+/* -
+
+add for each loop to add on hover, change color 
+need to figure out logic as to how to fill the grid correctly
+if the div is smaller, it doesnt fill the view vertically, 
+only horizontally
+- */
