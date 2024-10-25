15ec79349190f5c9cd14538c35728d2654f50994
Federico Olivares <119693761+FedeOlivares@users.noreply.github.com>
2024-10-25 19:42:16 +0000

    Add basic Js logic to create divs

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-c
+const view = document.querySelector(".view"); 
+const inp = document.getElementById("myRange");
+const side = inp.value; 
+
+const totalDivs = side * side; 
+
+
+const slider = document.querySelector(".slider");
+
+
+function updateSliderValue() {
+    document.documentElement.style.setProperty('--slider-value', `${slider.value}px`);
+}
+
+slider.addEventListener("input", updateSliderValue);
+
+
+ window.onload = () => {
+    for (let i = 1; i<=totalDivs; i++) {
+        const div = document.createElement("div");
+        div.classList.add("functionDiv");
+        view.appendChild(div);
+    }
+}
