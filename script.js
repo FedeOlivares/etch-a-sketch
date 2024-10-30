1f352d15c95a281258defcdfab10668db7352e59
Federico Olivares <119693761+FedeOlivares@users.noreply.github.com>
2024-10-30 19:04:23 +0000

    Add bubble with content that includes side length

--- script.js
+++ script.js
@@ -1,33 +1,55 @@
-const view = document.querySelector(".view"); 
+const view = document.querySelector(".view");
 const inp = document.getElementById("myRange");
 let side = inp.value;
-let totalDivs = side * side; 
-
+let totalDivs = side * side;
 
 const slider = document.querySelector(".slider");
+const sliderValueBubble = document.getElementById("sliderValueBubble"); // Bubble element
 
 function createDivs() {
     side = inp.value;
     totalDivs = side * side; 
     view.innerHTML = "";
-    for (let i = 1; i<=totalDivs; i++) {
+    for (let i = 1; i <= totalDivs; i++) {
         const div = document.createElement("div");
         div.classList.add("functionDiv");
         div.innerText = "";
         view.appendChild(div);
     }
+    const functionalDivs = document.querySelectorAll(".functionDiv");
+    functionalDivs.forEach((div) => {
+        div.addEventListener("mouseover", () => setColor(div));
+    });
 }
 
 function updateSliderValue() {
     view.style.setProperty('--slider-value', slider.value);
     createDivs();
+    updateBubble(); // Update bubble as well
 }
 
-slider.addEventListener("input", updateSliderValue);
+function updateBubble() {
+    const value = slider.value;
+    sliderValueBubble.innerText = value;
+    
+    // Calculate the position of the bubble relative to the slider's thumb
+    const percent = (value - slider.min) / (slider.max - slider.min);
+    sliderValueBubble.style.left = `calc(${percent * 100}% - 20px)`;
+}
 
+// Add event listeners
+slider.addEventListener("input", () => {
+    updateSliderValue();
+    updateBubble();
+});
 
+// Set initial states when the page loads
 window.onload = () => { 
     view.style.setProperty('--slider-value', slider.value);
-    createDivs();};
+    createDivs();
+    updateBubble();
+}
 
-
+function setColor(div) {
+    div.style.backgroundColor = "black";
+}
