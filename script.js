a1fa641959294659426015e09cdc384e096b7bfd
Federico Olivares <119693761+FedeOlivares@users.noreply.github.com>
2024-11-29 13:56:16 +0000

    Adds check to check if random color is selected
    Missing still JS function to change from false to true.

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const view = document.querySelector(".view");
 const inp = document.getElementById("myRange");
 const color = document.getElementById("randomColor");
-let side = inp.value;
-let totalDivs = side * side;
-
 const slider = document.querySelector(".slider");
 const sliderValueBubble = document.getElementById("sliderValueBubble"); // Bubble element
+
+let randomColor = color.value;
+let side = inp.value;
+let totalDivs = side * side;
 
 function createDivs() {
     side = inp.value;
@@ -13,18 +14,22 @@ function createDivs() {
     view.innerHTML = "";
     for (let i = 1; i <= totalDivs; i++) {
         const div = document.createElement("div");
-        div.classList.add("functionDiv");
+        div.classList.add("functionDiv")
+        div.style.backgroundColor = "whitesmoke";
         div.innerText = "";
         view.appendChild(div);
     }
     const functionalDivs = document.querySelectorAll(".functionDiv");
     functionalDivs.forEach((div) => {
         div.addEventListener("mouseover", () => {
-            if (div.style.backgroundColor != "black") {
+            if (randomColor == true) { // if button value true, sets random color instead of black
+                setRandomColor(div);
+            } else if (div.style.backgroundColor == "whitesmoke") {
                 setColor(div);
-            } else if (div.style.opacity < 1) {
+            }
+            if (div.style.opacity < 1) {
                 increaseOpacity(div);
-            };
+            }
         });
     });
 }
@@ -66,4 +71,15 @@ function increaseOpacity(div) {
     let currentOpacity = window.getComputedStyle(div).getPropertyValue('opacity');
     let newOpacity = parseFloat(currentOpacity) + 0.1;
     div.style.opacity = newOpacity;
+}
+
+
+function setColor(div) {
+    div.style.backgroundColor = "black";
+    div.style.opacity = 0.1;
+}
+
+function setRandomColor(div) {
+        const hex = Math.floor(Math.random() * 16777215).toString(16); // Random hex (0-16777215)
+        div.style.backgroundColor = `#${hex.padStart(6, '0')}`; // Ensure the result is always 6 digits
 }
